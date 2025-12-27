98704e53e156b8ada042bc7dc6c26b49899744a6
郑德胜 <2803919241@qq.com>
2025-12-27 20:56:01 +0800

    change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -35,6 +36,24 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+            excludes += "**/libimage_processing_util_jni.so"
+        }
+        resources {
+            excludes += "META-INF/DEPENDENCIES"
+            excludes += "META-INF/LICENSE"
+            excludes += "META-INF/LICENSE.txt"
+            excludes += "META-INF/NOTICE"
+            excludes += "META-INF/NOTICE.txt"
+        }
+    }
 }
 
 dependencies {
@@ -43,6 +62,41 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.runtime)
+    
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    
+    implementation(libs.retrofit)
+implementation(libs.retrofit.gson)
+implementation(libs.okhttp)
+implementation(libs.okhttp.logging)
+implementation(libs.gson)
+    
+    implementation(libs.glide)
+    
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    
+    implementation(libs.camerax.core)
+    implementation(libs.camerax.camera2)
+    implementation(libs.camerax.lifecycle)
+    implementation(libs.camerax.view)
+    
+    implementation(libs.swiperefresh)
+    implementation(libs.cardview)
+    
+    implementation("com.baidu.lbsyun:BaiduMapSDK_Map:7.6.4")
+    implementation("com.baidu.lbsyun:BaiduMapSDK_Search:7.6.4")
+    implementation("com.baidu.lbsyun:BaiduMapSDK_Location:9.6.4")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/zhengdesheng/z202304100318/ademo/MainActivity.kt
+++ app/src/main/java/com/zhengdesheng/z202304100318/ademo/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.zhengdesheng.z202304100318.ademo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.zhengdesheng.z202304100318.ademo.databinding.ActivityMainBinding
+import com.zhengdesheng.z202304100318.ademo.ui.diary.DiaryListActivity
+import com.zhengdesheng.z202304100318.ademo.ui.map.MapActivity
+import com.zhengdesheng.z202304100318.ademo.ui.profile.ProfileActivity
+import com.zhengdesheng.z202304100318.ademo.ui.shop.ShopListActivity
+import com.zhengdesheng.z202304100318.ademo.ui.wheel.WheelActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.cardWheel.setOnClickListener {
+            Log.d("MainActivity", "Wheel clicked")
+            startActivity(Intent(this, WheelActivity::class.java))
+        }
+
+        binding.cardShops.setOnClickListener {
+            Log.d("MainActivity", "Shops clicked")
+            startActivity(Intent(this, ShopListActivity::class.java))
+        }
+
+        binding.cardDiaries.setOnClickListener {
+            Log.d("MainActivity", "Diaries clicked")
+            startActivity(Intent(this, DiaryListActivity::class.java))
+        }
+
+        binding.cardMap.setOnClickListener {
+            Log.d("MainActivity", "Map clicked - starting MapActivity")
+            try {
+                val intent = Intent(this, MapActivity::class.java)
+                startActivity(intent)
+                Log.d("MainActivity", "MapActivity started successfully")
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Failed to start MapActivity", e)
+            }
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> true
+                R.id.navigation_shops -> {
+                    startActivity(Intent(this, ShopListActivity::class.java))
+                    true
+                }
+                R.id.navigation_diaries -> {
+                    startActivity(Intent(this, DiaryListActivity::class.java))
+                    true
+                }
+                R.id.navigation_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
